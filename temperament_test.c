e94839b0cb5c4efde2988d9fe8dc3cf3f1540044
Dirga Yuza <dirgayuza123@gmail.com>
2024-09-28 18:36:25 +0700

    temperament_tect.c: Added a basic welcome screen

--- temperament_test.c
+++ temperament_test.c
@@ -1,6 +1,28 @@
+/*
+*    temperament_test.c - THE TEMPERAMENTS TEST
+*
+*    THIS PROGRAM IS MADE AND WRITTEN BY:
+*    1. Gilbert Owen
+*    2. Raden Zahra Auliya Marissa Solihin
+*    3. Dirga Yuza
+*    4. Zefanya Felicita Adithya
+*    5. Shafiya Ramadhani Kanata
+*/
+
 #include <stdio.h>
+#define CLEAR_SCREEN "\e[1;1H\e[2J"
 
 int main(void) {
-    puts("Hello Sanguine!");
+    printf(CLEAR_SCREEN);
+    puts("*************************************************************************************");
+    puts("Selamat Datang di Tes Kepribadian The Four Temperaments!");
+    puts("Tekan Enter untuk melanjutkan, Tekan CTRL+C Kapan Saja untuk Memberhentikan Tes.");
+    puts("*************************************************************************************");
+ 
+    while (getchar() != '\n') {
+        getchar();
+    }
+
+    puts("Melanjutkan...");
     return 0;
 }
